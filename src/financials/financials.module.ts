4eff68a80673d29e815298c6c1f4c74eb4c1589e
wilsoncantin81 <wilsoncantin@gmail.com>
2026-03-30 21:22:40 -0500

    Update financials.module.ts

--- src/financials/financials.module.ts
+++ src/financials/financials.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
 import { FinancialsController } from './financials.controller';
 import { FinancialsService } from './financials.service';
-@Module({ controllers: [FinancialsController], providers: [FinancialsService] })
+@Module({ controllers: [FinancialsController], providers: [FinancialsService], exports: [FinancialsService] })
 export class FinancialsModule {}
